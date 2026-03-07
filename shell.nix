be976593098ecf736f9f7b31a01acb39c7ec75f1
David Sharpe <ds0196@uah.edu>
2026-03-06 21:41:32 -0600

    Fix indentation in zshrc

--- shell.nix
+++ shell.nix
@@ -17,13 +17,15 @@
     initContent =
       let
         zshConfigEarlyInit = lib.mkOrder 500 ''
-	  # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+          # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
           # Initialization code that may require console input (password prompts, [y/n]
           # confirmations, etc.) must go above this block; everything else may go below.
           if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
             source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
           fi
-	'';
+
+          path+="$HOME/.local/bin"
+          '';
         zshConfig = lib.mkOrder 1000 ""; 
       in
         lib.mkMerge [ zshConfigEarlyInit zshConfig ];
