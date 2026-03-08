a2eaf54733e9d819976b7bba9a72b0d5900c878e
David Sharpe <ds0196@uah.edu>
2026-03-07 23:27:47 -0600

    Fixed python env stuff, pyserial now accessible

--- packages/ros2.nix
+++ packages/ros2.nix
@@ -1,32 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.packages =
-    (with pkgs; [
-      colcon
-      (
-        with rosPackages.humble;
-        buildEnv {
-          paths = [
-            #desktop
-            ros-base
-            xacro
-            ament-cmake-core
-            python-cmake-module
-            ros2-control
-            ros2-controllers
-            control-msgs
-            robot-state-publisher
-            rqt-graph
-            rmw-cyclonedds-cpp
-            zed-msgs
-          ];
-        }
+  home.packages = with pkgs; [
+    colcon
+    (
+      with rosPackages.humble;
+      buildEnv {
+        paths = [
+          ros-base
+          xacro
+          ament-cmake-core
+          python-cmake-module
+          ros2-control
+          ros2-controllers
+          control-msgs
+          robot-state-publisher
+          rqt-graph
+          rmw-cyclonedds-cpp
+          zed-msgs
+        ];
+      }
+    )
+    # I put a list in your set in list in set
+    (lib.hiPrio (
+      pkgs.python313.withPackages (
+        ps: with ps; [
+          pyserial
+          pygame
+          scipy
+          crccheck
+        ]
       )
-    ])
-    ++ (with pkgs.python313Packages; [
-      pyserial
-      pygame
-      scipy
-      crccheck
-    ]);
+    ))
+  ];
 }
