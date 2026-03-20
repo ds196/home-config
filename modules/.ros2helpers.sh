4160990ea1a9042a2b0983218283b02fa5835e86
David <ds0196@uah.edu>
2026-03-20 14:48:16 -0500

    Fix ROS2 aliases

--- modules/.ros2helpers.sh
+++ modules/.ros2helpers.sh
@@ -14,26 +14,22 @@ ROS_DISTRO=humble  # Default to Humble if not on Ubuntu
 
 CURR_SHELL=$(ps -cp "$$" -o command="")
 
-# Just source, but build if needed
-alias sw='start_ws'
-# Build and source
-alias bs='verify_ws $PWD && buildws && sourcews'
-alias cbs='verify_ws $PWD && rm -rf build/ install/ || bs'
-
 
 # Source main ROS2 underlay
-if [ -d "/opt/ros/$ROS_DISTRO" ]; then
-	source /opt/ros/$ROS_DISTRO/setup.$CURR_SHELL
-else
-	if [ -d /opt/ros -a -z $(ls /opt/ros 2>/dev/null) ]; then
-		echo "[$FILENAME] Available ROS distros: $(ls /opt/ros)"
-	fi
+if command -v ros2 >/dev/null 2>&1; then
+    if [ -d "/opt/ros/$ROS_DISTRO" ]; then
+    	source /opt/ros/$ROS_DISTRO/setup.$CURR_SHELL
+    else
+    	if [ -d /opt/ros -a -z $(ls /opt/ros 2>/dev/null) ]; then
+    		echo "[$FILENAME] Available ROS distros: $(ls /opt/ros)"
+    	fi
+    fi
 fi
 
 # Verify that $1 is a valid ROS workspace
 verify_ws() {
 	# Use PWD by default
-	[ -z "$1" ] && DIR="$PWD" || DIR="$1"
+    DIR=${1:-$PWD}
 
 	# Make sure this is a ros2 workspace
 	if [ ! -d "$DIR" -o $(basename "$DIR") = "src" -o ! -d "$DIR/src" -o "$DIR" = "$HOME" ]; then
@@ -42,23 +38,31 @@ verify_ws() {
 	fi
 }
 
+# Build a workspace. Mainly just here to keep the flags in one spot.
+# This is an alias for adding addl. flags
+alias buildws='/usr/bin/env colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
+
 # Change directories into $1, build if required, and source setup
 start_ws() {
 	# Use PWD by default
-	[ -z "$1" ] && DIR="$PWD" || DIR="$1"
+    DIR=${1:-$PWD}
 
 	if ! (verify_ws "$DIR"); then
 		return 1;
 	fi
 
-	cd $DIR
+	if [[ "$PWD" != "$DIR" ]]; then
+        cd $DIR
+    fi
+    #echo "PATH: $PATH"
+    echo "COLCON_PREFIX_PATH: $COLCON_PREFIX_PATH"
+    echo "colcon: $(which colcon)"
 
-	[ -f "./install/setup.$CURR_SHELL" ] && sourcews || bs
+	if [ ! -f "./install/setup.$CURR_SHELL" ]; then
+        buildws
+    fi
+    sourcews
 }
-
-# Build a workspace. Mainly just here to keep the flags in one spot.
-# This is an alias for adding addl. flags
-alias buildws='colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
 
 # Just source a workspace. Just source it. Includes sourced workspaces when building.
 function sourcews {
@@ -67,4 +71,11 @@ function sourcews {
     fi
     source "install/setup.$CURR_SHELL"
 }
+
+
+# Just source, but build if needed
+alias sw='start_ws'
+# Build and source
+alias bs='verify_ws $PWD && buildws && sourcews'
+alias cbs='verify_ws $PWD && rm -rf build/ install/ || bs'
 
